3c20f59d81fde891626fe692828ca2ad74d81812
mert18 <mertuygur02@gmail.com>
2022-07-01 20:28:34 +0300

    feat: methods added get, post, update and delete methods implemented

---
+++ src/main/java/org/acme/MovieResource.java
@@ -0,0 +1,70 @@
+package org.acme;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/movies")
+public class MovieResource {
+  public static List<Movie> movies = new ArrayList<>();
+  
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getMovies() {
+    return Response.ok(movies).build();
+  }
+
+  @GET
+  @Produces(MediaType.TEXT_PLAIN)
+  @Path("/size")
+  public Integer countMovies() {
+    return movies.size();
+  }
+
+  @POST
+  @Produces(MediaType.APPLICATION_JSON)
+  @Consumes(MediaType.APPLICATION_JSON)
+  public Response createMovie(Movie newMovie){
+    movies.add(newMovie);
+    return Response.ok(movies).build();
+  }
+
+  @PUT
+  @Path("{id}/{title}")
+  @Produces(MediaType.APPLICATION_JSON)
+  @Consumes(MediaType.APPLICATION_JSON)
+  public Response updateMovie(
+          @PathParam("id") Long id,
+          @PathParam("title") String title){
+    movies = movies.stream().map(movie -> {
+      if(movie.getId().equals(id)){
+        movie.setTitle(title);
+      }
+      return movie;
+    }).collect(Collectors.toList());
+    return Response.ok(movies).build();
+  }
+
+
+  @DELETE
+  @Path("{id}")
+  @Consumes(MediaType.APPLICATION_JSON)
+  public Response deleteMovie(@PathParam("id") Long id){
+    Optional<Movie> movieToDelete = movies.stream().filter(movie -> movie.getId().equals(id)).findFirst();
+    boolean removed = false;
+
+    if(movieToDelete.isPresent()){
+      removed = movies.remove(movieToDelete.get());
+    }
+
+    if(removed){
+      return Response.noContent().build();
+    }
+    return Response.status(Response.Status.BAD_REQUEST).build();
+  }
+}
